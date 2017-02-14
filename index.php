d1e5932bb4795da023169d78b8d15311eac32d46
briangrimson <leifgrimson@gmail.com>
2017-02-14 12:26:23 -0500

    cleaned up code

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 $list=array("There is nothing to fear but fear itself - FDR", "I beleive I can fly - R Kelly", "I'm hungry - Dave");
 $random = array_rand($list);
 
-echo "
-<!doctype html>
+echo 
+"<!doctype html>
 <html>
 <head>
 <link rel=stylesheet href=css/site.css>
@@ -24,8 +24,7 @@ My name is Brian Grimson. I am an artist currently living in the boston area. Yo
 </p> 
 <p class=quote>".$list[$random]."</p>
 </body>
-</html>"    
-    ;
+</html>";
     
 
 ?>
